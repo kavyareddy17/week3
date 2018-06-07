d89c6fa3c251fc5ee68cad809a688998d80bdf8d
Kavya Reddy Shivampet <kavyareddys@virtusa.com>
2018-06-07 15:35:57 +0530

    week3

---
+++ form/application/js/app.js
@@ -0,0 +1,34 @@
+
+function addDetails(){
+    console.log("");
+    if(validateFirstName()){
+        if(validateFulName()){
+            //post it
+        }
+        else {
+            //display error
+        }
+    }
+    else{
+        //display error
+    }
+}
+
+function validateFirstName(){
+ var firstName=$()
+}
+
+function validateFulName(){
+return true;
+}
+
+//$(document).ready(function(){
+  //  var date_input=$('input[name="date"]');
+    //var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent():"body";
+//    date_input.datepicker({
+  //      format: 'mm/dd/yyyy',
+     //   container:container,
+       // todayHighLight: true ,
+   //     autoclose: true ,
+   // })
+//})
